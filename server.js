4f845198da63e51dd9928a2d90e937f3fb41df26
Bianca Barros <barrosbianca972@gmail.com>
2025-07-14 12:12:16 -0300

    versao final

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({extended: true}));
+
+let ultimocontato = null; 
+
+app.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contato.html'));
+});
+
+app.post('/contato', (req, res) => {
+   ultimocontato = req.body;
+    if(!ultimocontato){
+      res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+  }
+    const {nameC, email, assunto, mensagem} = ultimocontato;
+    res.status(200).send (`
+        <h1>Contato recebido, obrigado!</h1>
+        <p>Nome:<strong>${nameC}</strong></p>
+        <p>Email:<strong>${email}</strong></p>
+        <p>Assunto:<strong>${assunto}</strong></p>
+        <p>Mensagem:<strong>${mensagem}</strong></p>
+        `);
+  
+});
+
+let ultimasugestao = null; 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/sugestao', (req, res)=>{
+    const name = req.query.name;
+    const ingredientes = req.query.ingredientes;
+
+    if(!name && !ingredientes){
+         res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    }
+    res.status(200).send(`
+        <h1>Sugestão recebida, obrigado!</h1>
+        <p>Nome:<strong>${name}</strong></p>
+        <p>ingredientes:<strong>${ingredientes}</strong></p>
+        `);
+})
+
+app.get('/api/lanches', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'data/lanches.json'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
+});
+
